5d47f2c2ce724c3f97bbed0d494b522a8b2efbc0
zenonet <78077158+zenonet@users.noreply.github.com>
2024-07-27 15:39:54 +0200

    Added naturally spawning animals

--- animal.gd
+++ animal.gd
@@ -1,24 +1,33 @@
 extends Node3D
 
-var currentSpeed = 12.5
+var currentSpeed = 12
 const avoidanceDistance = 1.2
 @export var isPlayerControlled:bool = false
 
-const SPEED = 8
+const HORIZONTAL_SPEED = 8
 
 @onready var targetX:float = position.x 
 @onready var playerPosition = $PlayerPosition.position
+
+func _ready():
+	# Leave the moving part of the world
+	if get_parent().is_in_group("chunks"):
+		# print("Reparenting animal")
+		var z = global_position.z
+		reparent($"../../..")
+		global_position.z = z
+
 func _process(delta):
 	if isPlayerControlled:
 		var input = Input.get_axis("right", "left")
-		position.x += input * SPEED * delta
+		position.x += input * HORIZONTAL_SPEED * delta
 	else:
 		var relativeSpeed:float = currentSpeed - GameManager.playerSpeed
-		global_position.z += relativeSpeed*delta
+		print("relative speed: %f; world speed: %f" % [relativeSpeed, GameManager.playerSpeed])
+		position.z += relativeSpeed*delta
 		position.x = move_toward(position.x, targetX, 4*delta)
 
 func on_avoidance_trigger_detected(direction:int):
-	print("avoiding obstacle...")
 	targetX += direction * avoidanceDistance
 
 
