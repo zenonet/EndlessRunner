cc875d9233287c9f61c5b8afeb0992b99fbf5390
zenonet <78077158+zenonet@users.noreply.github.com>
2024-07-27 16:35:29 +0200

    Finally made animal spawning good and improved world generation

--- animal.gd
+++ animal.gd
@@ -1,37 +1,37 @@
 extends Node3D
 
-var currentSpeed = 12
+@onready var normalSpeed:float = randf_range(6, 12)
 const avoidanceDistance = 1.2
 @export var isPlayerControlled:bool = false
 
 const HORIZONTAL_SPEED = 8
 
-@onready var targetX:float = position.x 
+@onready var targetX:float = global_position.x 
 @onready var playerPosition = $PlayerPosition.position
-
-func _ready():
-	# Leave the moving part of the world
-	if get_parent().is_in_group("chunks"):
-		# print("Reparenting animal")
-		var z = global_position.z
-		reparent($"../../..")
-		global_position.z = z
 
 func _process(delta):
 	if isPlayerControlled:
 		var input = Input.get_axis("right", "left")
 		position.x += input * HORIZONTAL_SPEED * delta
 	else:
-		var relativeSpeed:float = currentSpeed - GameManager.playerSpeed
-		print("relative speed: %f; world speed: %f" % [relativeSpeed, GameManager.playerSpeed])
+		var relativeSpeed:float = normalSpeed - GameManager.playerSpeed
+		# print("relative speed: %f; world speed: %f" % [relativeSpeed, GameManager.playerSpeed])
 		position.z += relativeSpeed*delta
-		position.x = move_toward(position.x, targetX, 4*delta)
+		global_position.x = move_toward(global_position.x, targetX, 4*delta)
 
 func on_avoidance_trigger_detected(direction:int):
 	targetX += direction * avoidanceDistance
 
 
 func _on_body_entered(body):
+	if !isPlayerControlled: return
 	# if object is obstacle
-	if isPlayerControlled and body.is_in_group("obstacles"):
+	if body.is_in_group("obstacles"):
+		GameManager.game_over.emit()
+
+
+func _on_area_entered(area):
+	if !isPlayerControlled: return
+	
+	if (area.is_in_group("animals") && !area.isPlayerControlled):
 		GameManager.game_over.emit()
--- worldGenerator.gd
+++ worldGenerator.gd
@@ -1,12 +1,13 @@
 extends Node3D
 
+var animal:PackedScene = preload("res://scenes/obstacles/animal.tscn")
 @export var chunkScenes:Array[PackedScene]
 @export var chunkCount:float = 3
 @export var chunkLength:float = 4
 
 func _ready():
 	for i in range(chunkCount):
-		generate_chunk(i, 0 if i < 3 else -1)
+		generate_chunk(i, 0 if i == 0 else -1)
 
 func generate_chunk(offset:int = chunkCount/2, chunkId:int = -1):
 	var chunk_to_create:PackedScene = chunkScenes.pick_random() if chunkId == -1 else chunkScenes[chunkId]
@@ -15,9 +16,17 @@ func generate_chunk(offset:int = chunkCount/2, chunkId:int = -1):
 	self.add_child(chunk)
 	chunk.position.z = offset*chunkLength
 	
+	for c in chunk.get_children():
+		if c.is_in_group("spawn") && randi()%5 == 0:
+			print("Spawing animal in chunk %d at z=%d" % [offset, chunk.global_position.z])
+			var a = animal.instantiate()
+			get_parent().add_child.call_deferred(a)
+			a.global_position = c.global_position
+			a.targetX = a.position.x
+	
 func _process(delta):
 	GameManager.distance += GameManager.playerSpeed*delta
-	GameManager.playerSpeed += delta * 0.25
+	# GameManager.playerSpeed += delta * 0.25
 	for c in get_children():
 		c.position.z -= GameManager.playerSpeed*delta
 		if c.position.z < -chunkLength*2:
